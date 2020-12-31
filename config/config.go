b80f7227f09e66e53d14fcd723693dd1464e9d4c
Ahmad Anvari <ahmad.anv@gmail.com>
2020-12-31 22:46:09 +0330

    Create config

---
+++ config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	BasePath string `mapstructure:"base_path"`
+}
+
+var C *Config
+
+func InitConfig() (*Config, error) {
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.SetConfigType("yml")    // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath(".")      // optionally look for config in the working directory
+	err := viper.ReadInConfig()   // Find and read the config file
+	if err != nil {               // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	var c Config
+	err = viper.Unmarshal(&c)
+	if err != nil {
+		return nil, err
+	}
+
+	C = &c
+
+	return &c, nil
+}
