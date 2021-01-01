8cc522ff2c796d12da9bc64b61c404569cf1c99e
Ahmad Anvari <ahmad.anv@gmail.com>
2021-01-01 09:51:02 +0330

    Init config and add static serve handlers

--- cmd/serve.go
+++ cmd/serve.go
@@ -7,6 +7,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/spf13/cobra"
 
+	"github.com/anvari1313/photopic-server/config"
 	"github.com/anvari1313/photopic-server/routes"
 )
 
@@ -19,13 +20,20 @@ var serveCMD = &cobra.Command{
 }
 
 func serve() {
+	_, err := config.InitConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	e := echo.New()
 	e.HideBanner = true
 	e.Use(middleware.Logger())
 
 	e.GET("/list", routes.ListHandler)
+	e.Static("/thumbnail", config.C.BasePath)
+	e.Static("/static", config.C.BasePath)
 
-	if err := e.Start(":8080"); err != nil {
+	if err := e.Start(config.C.Address); err != nil {
 		log.Fatal(err)
 	}
 }
